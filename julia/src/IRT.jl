0b4e8bd836940e794041dbc65b3b0650f64ed99c
Lee Goddard <leegee@gmail.com>
2025-09-19 13:37:05 +0200

    Split more out

---
+++ julia/src/IRT.jl
@@ -0,0 +1,33 @@
+module IRTUtils
+
+import Turing
+import Distributions
+import StatsFuns
+import Base.Threads
+
+export fit_irt_model
+
+function fit_irt_model(response_matrix::Matrix{Int})
+    n_demo, n_items_total = size(response_matrix)
+
+    Turing.@model function irt_model(response_matrix)
+        θ ~ Turing.MvNormal(zeros(n_demo), ones(n_demo))
+        b ~ Turing.MvNormal(zeros(n_items_total), ones(n_items_total))
+        logit_p = θ .- transpose(b)
+        p = @. StatsFuns.logistic(logit_p)
+        for i in 1:n_demo, j in 1:n_items_total
+            response_matrix[i, j] ~ Distributions.Bernoulli(p[i, j])
+        end
+    end
+
+    model = irt_model(response_matrix)
+    n_chains = Threads.nthreads() > 1 ? Threads.nthreads() : 4
+    chain = Turing.sample(model, Turing.NUTS(0.65), 1000;
+        tune=500,
+        chains=n_chains,
+        progress=true,
+        threaded_chains=true)
+    return chain
+end
+
+end # module
--- julia/src/LLMBiasAudit.jl
+++ julia/src/LLMBiasAudit.jl
@@ -1,10 +1,10 @@
 module LLMBiasAudit
 
+include("IRT.jl")
+import .IRT
+
 import DataFrames
 import CSV
-import Turing
-import Distributions
-import StatsFuns
 import PromptingTools
 import JLD2
 import Base.Threads
@@ -127,23 +127,6 @@ function classify_responses_llm(responses_text::Vector{String}; model::String="g
     return response.content, binary
 end
 
-function fit_irt_model(response_matrix::Matrix{Int})
-    n_demo, n_items_total = size(response_matrix)
-    Turing.@model function irt_model(response_matrix)
-        θ ~ Turing.MvNormal(zeros(n_demo), ones(n_demo))
-        b ~ Turing.MvNormal(zeros(n_items_total), ones(n_items_total))
-        logit_p = θ .- transpose(b)
-        p = @. StatsFuns.logistic(logit_p)
-        for i in 1:n_demo, j in 1:n_items_total
-            response_matrix[i, j] ~ Distributions.Bernoulli(p[i, j])
-        end
-    end
-    model = irt_model(response_matrix)
-    n_chains = Threads.nthreads() > 1 ? Threads.nthreads() : 4
-    chain = Turing.sample(model, Turing.NUTS(0.65), 1000; tune=500, chains=n_chains, progress=true, threaded_chains=true)
-    return chain
-end
-
 function main(; models=default_models)
     demographics = define_demographics()
     items = define_items()
@@ -177,7 +160,7 @@ function main(; models=default_models)
 
         # Fit IRT
         response_matrix = reshape(responses_bin, length(demographics), length(items))
-        chain = fit_irt_model(response_matrix)
+        chain = IRT.fit_irt_model(response_matrix)
         filename_chain = "jld2/irt_chain_$(safe_model_name).jld2"
         JLD2.@save filename_chain chain
         println("IRT chain saved to '$(filename_chain)'")
